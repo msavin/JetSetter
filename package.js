4eaf14983d0da1b153cb1bf3819a26278cda996d
Max Savin <msavin@me.com>
2015-03-25 16:58:24 -0400

    Updated readme

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name:    'msavin:jetsetter',
   summary: 'Visual Get/Set Tool for Sessions',
-  version: '1.0.0',
+  version: '1.0.2',
   git:     'https://github.com/msavin/JetSetter.git',
   documentation: 'README.md',
   debugOnly: true
